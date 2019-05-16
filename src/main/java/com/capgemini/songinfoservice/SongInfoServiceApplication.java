6218d554e6ce9304d7540d8360a2bfe04cc9e251
Your Name <you@example.com>
2019-05-16 10:52:32 +0530

    added

---
+++ src/main/java/com/capgemini/songinfoservice/SongInfoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.songinfoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongInfoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongInfoServiceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/capgemini/songinfoservice/controller/SongController.java
@@ -0,0 +1,52 @@
+package com.capgemini.songinfoservice.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.capgemini.songinfoservice.entity.Song;
+import com.capgemini.songinfoservice.entity.SongList;
+import com.capgemini.songinfoservice.service.SongService;
+
+@RestController
+@CrossOrigin("*")
+public class SongController {
+
+	@Autowired
+	private SongService songService;
+
+	@PostMapping("/song")
+	public ResponseEntity<Song> addNewSong(@RequestBody Song song) {
+		Song song2 = songService.addNewSong(song);
+		return new ResponseEntity<Song>(song2, HttpStatus.OK);
+	}
+
+	@GetMapping("/song")
+	public ResponseEntity<List<Song>> getAllSongs() {
+		List<Song> songs = songService.getAllSongs();
+		return new ResponseEntity<List<Song>>(songs, HttpStatus.OK);
+	}
+
+	@GetMapping("/song/album/{albumId}")
+	public ResponseEntity<SongList> getAllSongsByAlbumId(@PathVariable int albumId) {
+		SongList songs = new SongList();
+		songs.setSongs(songService.getAllSongsByAlbmId(albumId));
+		return new ResponseEntity<SongList>(songs, HttpStatus.OK);
+	}
+
+	@GetMapping("/song/artist/{artistName}")
+	public ResponseEntity<SongList> getAllSongsByArtistName(@PathVariable String artistName) {
+
+		SongList songs = new SongList();
+		songs.setSongs(songService.getAllSongsByArtistName(artistName));
+		return new ResponseEntity<SongList>(songs, HttpStatus.OK);
+	}
+}
